1caa8267c9165cdde8c43daa47f69951b168943a
ikigai27 <tanvishindodkar44@gmail.com>
2025-11-11 18:58:32 +0530

    init

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../components/Card";
 import { Globe } from "../components/globe";
 import { mySocials } from "../constants";
-import { Frameworks } from "../components/FrameWorks";
+import { Frameworks } from "../components/Frameworks";
 
 const About = () => {
   const grid2Container = useRef();
